7132b32d9bb752a7fd90d31bdeb0c45fcc1ab4cb
cnunciato <cnunciato@rhapsody.com>
2012-09-10 09:34:19 -0700

    readme

--- _plugins/soundcloud_sound.rb
+++ _plugins/soundcloud_sound.rb
@@ -7,7 +7,7 @@
 #    {% soundcloud_sound 12345 widgetname ffffff %}
 #    {% soundcloud_sound 12345 widgetname ffffff small %}
 #
-#    ... where 12345 is the SoundCloud sound ID, and widgetname is the sound's visual representation.
+#    where 12345 is the SoundCloud sound ID, widgetname is the sound's visual representation, ffffff is the color, and size is, well, the size (SoundCloud gives you three options for the "artwork" widget).
 #
 #  Available SoundCloud widgets:
 #   
@@ -16,7 +16,7 @@
 #    mini *
 #    artwork *
 #    
-#      * Requires a paid SoundCloud tier
+#    * Requires a paid SoundCloud tier
 #
 #  Author: Chris Nunciato (@cnunciato)
 #  Source: http://github.com/cnunciato/jekyll-soundcloud
